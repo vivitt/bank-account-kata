b0ed351ef67bef82dba836b2b805a8ae134d410a
vivitt <viviviyanez@gmail.com>
2024-07-29 01:05:46 +0200

    create next project and add tailwind

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold underline">Bank account kata</h1>
+    </main>
+  );
+}
